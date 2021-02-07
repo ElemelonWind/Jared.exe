9753d9392c28d60d0796c232872bee81a937b485
BuildTools <unconfigured@null.spigotmc.org>
2021-02-07 16:35:09 -0500

    http

--- Jared.js
+++ Jared.js
@@ -1,4 +1,8 @@
 const Discord = require('discord.js');
+var http = require('http');  
+http.createServer(function (req, res) {
+     res.writeHead(200, {'Content-Type': 'text/plain'});     
+     res.send('it is running\n'); }).listen(process.env.PORT || 5000); 
 //const mongo = require('./mongo');
 //const channelSchema = require('./schemas/channel-schema');
 
