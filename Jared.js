90cf11adbbba09fe61820e0656993e0741e6a217
Melon <74145775+ElemelonWind@users.noreply.github.com>
2021-10-06 20:38:01 -0400

    Update Jared.js

--- Jared.js
+++ Jared.js
@@ -246,7 +246,7 @@ client.on('message', (message) => {
 //}
 
 //const token = fs.readFileSync("token.txt").toString().trim();
-const token = secret.TOKEN
+const token = secrets.TOKEN
 client.login(token);
 
 function findIndex(guildId) {
